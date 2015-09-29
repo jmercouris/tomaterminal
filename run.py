cbfb38908d08a524aed1ee3e8068fe8bc0f9ff04
John Mercouris <jmercouris@gmail.com>
2015-09-29 00:52:35 -0500

    Added a readme and command line arguments

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 import time
 import sys
+import argparse
+
+# Help String
+description_string = "Tomaterminal is a terminal program based on the Pomodoro (Italian for Tomato) method of working. In the Pomodoro method, you take a timer ((frequently tomato shaped) historically used in kitchens) and you set a 25 minute timer for work. After 25 mintues are completed, you set a 5 minute timer for break. Tomaterminal emulates this exact behavior, alerting you after 25 minutes have elapsed, then after your 5 minute break has elapsed."
+
+parser = argparse.ArgumentParser(description=description_string)
+parser.add_argument('-t','--task_time', type=int, help='Task Interval (minutes)',required=False)
+parser.add_argument('-b','--break_time', type=int, help='Break Interval (minutes)',required=False)
+args = parser.parse_args()
 
 # Time Definitions
 seconds_minute = 1
@@ -10,6 +19,12 @@ hours_day = 24
 # Task Definitions
 task_time = 25
 break_time = 5
+
+# Override task/break time if command line arguments passed
+if args.task_time is not None:
+    task_time = args.task_time
+if args.break_time is not None:
+    break_time = args.break_time
 
 # UI Definitions
 progress_bar_length = 40
@@ -23,9 +38,6 @@ def progress(count, total, suffix=''):
     bar = '=' * filled_len + '-' * (progress_bar_length - filled_len)
     sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
     sys.stdout.flush()
-
-def print_help():
-    print("Help")
 
 # Initial Entry into Program; Clear Screen
 print(chr(27) + "[2J")
@@ -41,8 +53,3 @@ while True:
         time.sleep(seconds_minute)
         progress(i,break_time,'Break Time Elapsed: %s:00' % i)
     alert()
-
-
-
-
-
